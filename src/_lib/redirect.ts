4be5444464f010eb6bf7449b9f4f6bf8cd2ec1bd
Frank Harris <fcharris@gmail.com>
2024-12-26 21:57:42 -0500

    added email redirect

--- src/_lib/redirect.ts
+++ src/_lib/redirect.ts
@@ -5,6 +5,10 @@ export const definitions = {
     to: "https://www.paypal.com/donate/?hosted_button_id=RGMKR4SU8LN2S",
     code: 301,
   },
+  "/email": {
+    to: "mailto:kidsforcasabuna@gmail.com",
+    code: 301,
+  },
 };
 
 export default redirects({
